a2da6d71e9081e70a006183b1eba0ec4cdc78c28
mcarans <rans@email.com>
2025-05-21 10:05:57 +1200

    Token tester

---
+++ README.md
@@ -0,0 +1,1 @@
+# HDX API Token Tester
---
+++ src/hdx/scraper/tokentest/__main__.py
@@ -0,0 +1,37 @@
+"""Entry point to start HAPI HNO pipeline"""
+
+import logging
+from os.path import expanduser, join
+
+from ._version import __version__
+from hdx.data.user import User
+from hdx.facades.infer_arguments import facade
+from hdx.utilities.easy_logging import setup_logging
+
+setup_logging()
+logger = logging.getLogger(__name__)
+
+lookup = "hdx-scraper-tokentest"
+updated_by_script = "HDX Scraper: Token Test"
+
+
+def main(
+    save: bool = False,
+) -> None:
+    """Test token
+
+    Returns:
+        None
+    """
+    logger.info(f"##### {lookup} version {__version__} ####")
+    if not User.check_current_user_organization_access("hdx", "create_dataset"):
+        raise PermissionError("API Token does not give access to HDX organisation!")
+    logger.info("HDX Scraper token test completed!")
+
+
+if __name__ == "__main__":
+    facade(
+        main,
+        user_agent_config_yaml=join(expanduser("~"), ".useragents.yaml"),
+        user_agent_lookup=lookup,
+    )
